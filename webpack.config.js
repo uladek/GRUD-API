d69158878ea68e5c3dc84251b14f1002250725b5
Uladimir Hrydzin <hrydzin@gmail.com>
2024-02-13 02:51:00 +0400

    feat: add tart:prod to script

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,43 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+// import path from 'path';
+// import { fileURLToPath } from 'url';
+// import { dirname } from 'path';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
-const isProduction = process.env.NODE_ENV === 'production';
+// const isProduction = process.env.NODE_ENV === 'production';
+
+// const config = {
+//     entry: './src/index.ts',
+//     target: 'node',
+//     mode: isProduction ? 'production' : 'development',
+//     module: {
+//         rules: [
+//             {
+//                 test: /\.ts$/,
+//                 use: 'ts-loader',
+//                 exclude: /node_modules/,
+//             },
+//         ],
+//     },
+//     resolve: {
+//         extensions: ['.ts', '.js'],
+//     },
+//     output: {
+//         filename: 'bundle.js',
+//         path: path.resolve(__dirname, 'build'),
+//     },
+// };
+
+// export default config;
+
+
+const path = require('path');
 
 const config = {
     entry: './src/index.ts',
     target: 'node',
-    mode: isProduction ? 'production' : 'development',
+    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
     module: {
         rules: [
             {
@@ -29,4 +56,4 @@ const config = {
     },
 };
 
-export default config;
+module.exports = config;
