6da0681cd3000ae6462abb7019c26e4e780dfc7c
Uladimir Hrydzin <hrydzin@gmail.com>
2024-10-16 22:03:40 +0400

    refactor: config according standatr ES^

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,49 @@
-// import path from 'path';
-// import { fileURLToPath } from 'url';
-// import { dirname } from 'path';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-// const isProduction = process.env.NODE_ENV === 'production';
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const config = {
+    entry: './src/index.ts',
+    target: 'node',
+    mode: isProduction ? 'production' : 'development',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'build'),
+        // libraryTarget: 'module',
+        chunkFormat: 'commonjs',
+    },
+    experiments: {
+        outputModule: true,
+      },
+};
+
+export default config;
+
+
+// const path = require('path');
 
 // const config = {
 //     entry: './src/index.ts',
 //     target: 'node',
-//     mode: isProduction ? 'production' : 'development',
+//     mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
 //     module: {
 //         rules: [
 //             {
@@ -29,31 +62,4 @@
 //     },
 // };
 
-// export default config;
-
-
-const path = require('path');
-
-const config = {
-    entry: './src/index.ts',
-    target: 'node',
-    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.ts', '.js'],
-    },
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'build'),
-    },
-};
-
-module.exports = config;
+// module.exports = config;
